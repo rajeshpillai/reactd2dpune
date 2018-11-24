b7543c3ce1cd5582f5d00a431085829e4d6c1a25
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-24 12:53:09 +0530

    Added task toggle feature and delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const taskCompleteStyle = {
+  textDecoration: "line-through",
+};
+
+
 class App extends Component {
   constructor() {
     super();
     this.onTodoRemove = this.onTodoRemove.bind(this);
     this.onTaskAdd = this.onTaskAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
+      newTask: "",
       tasks: [
         { id: 1, "title": "Task 1", done: false },
         { id: 2, "title": "Task 2", done: true },
@@ -14,16 +21,22 @@ class App extends Component {
       ]
     }
   }
+  handleChange(e) {
+    this.setState({
+      newTask: e.target.value
+    });
+  }
 
   onTaskAdd() {
     let newTask = {
       id: this.state.tasks.length + 1,
-      title: +new Date(),
+      title: this.state.newTask,
       done: false
     }
     this.setState({
-      tasks: [...this.state.tasks, newTask]
-    })
+      tasks: [newTask, ...this.state.tasks],
+      newTask: ""
+    });
   }
 
   onTodoRemove(id) {
@@ -36,13 +49,31 @@ class App extends Component {
     });
   }
 
+  onToggleTask(taskId) {
+    let tasks = this.state.tasks.map((task) => {
+      if (task.id === taskId) {
+        task.done = !task.done;
+      }
+      return task;
+    });
+
+    this.setState({
+      tasks
+    });
+  }
   render() {
     let taskUI = this.state.tasks.map((task) => {
+      let taskStyle = {};
+      if (task.done) {
+        taskStyle = taskCompleteStyle;
+      }
       return (
-        <li key={task.id}>
+        <li key={task.id} style={taskStyle}
+          onDoubleClick={() => { this.onToggleTask(task.id) }}>
           <span>{task.title}
             <button onClick={() => { this.onTodoRemove(task.id) }} >X</button></span>
         </li>
+
       );
     });
 
@@ -50,6 +81,8 @@ class App extends Component {
     return (
       <div>
         <h2>Task App</h2>
+        <input onChange={this.handleChange}
+          value={this.state.newTask} type="text" placeholder="new task" />
         <button onClick={this.onTaskAdd}>New Task</button>
         <ul>
           {taskUI}
