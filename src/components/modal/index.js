8b592c256dfc712e7ffd4d7cb32970604bd62ed8
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-24 16:27:19 +0530

    Modal dialog

---
+++ src/components/modal/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+// gray background
+const backdropStyle = {
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    padding: 50
+}
+
+const modalStyle = {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    maxWidth: 500,
+    minHeight: 300,
+    margin: '0 auto',
+    padding: 30,
+    position: "relative"
+};
+
+const footerStyle = {
+    position: "absolute",
+    bottom: 20
+};
+export default class Modal extends React.Component {
+    onClose(e) {
+        this.props.onClose(e);
+    }
+
+    onKeyUp = (e) => {
+        // Lookout for ESC key (27)
+        console.log(e.which);
+        if (e.which === 27 && this.props.show) {
+            this.onClose(e);
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener("keyup", this.onKeyUp);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keyup", this.onKeyUp);
+    }
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+        return (
+            <div style={backdropStyle}>
+                <div style={modalStyle}>
+                    {this.props.children}
+                    <div style={footerStyle}>
+                        <button
+                            onClick={(e) => { this.onClose(e) }}>Close</button>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
--- src/components/task-list.js
+++ src/components/task-list.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import Task from './task';
+import Modal from './modal';
 
-export default function TaskList(props) {
-    let tasks = props.tasks;
-    let taskUI = tasks.map((task) => {
+export default class TaskList extends React.Component {
+    state = {
+        show: false
+    }
+
+    constructor() {
+        super();
+        this.showModal = this.showModal.bind(this);
+        this.onClose = this.onClose.bind(this);
+    }
+
+
+
+    showModal() {
+        this.setState({
+            show: true
+        });
+    }
+
+    onClose() {
+        this.setState({
+            show: false
+        });
+    }
+    render() {
+        let tasks = this.props.tasks;
+        let taskUI = tasks.map((task) => {
+            return (
+                <div>
+                    <Task key={task.id} data={task} />
+                    <button onClick={this.showModal}>Show Task</button>
+                    <Modal
+                        onClose={this.onClose}
+                        show={this.state.show}>
+                        <Task key={task.id} data={task} />
+                    </Modal>
+                </div>
+            );
+        });
         return (
-            <Task key={task.id} data={task} />
+            <ul>
+                {taskUI}
+            </ul >
         );
-    });
-    return (
-        <ul>
-            {taskUI}
-        </ul >
-    );
+    }
+
 }
