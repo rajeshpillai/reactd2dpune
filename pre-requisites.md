4924b73e0117f4ced06f90dc5a29065256d0d341
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-24 12:03:02 +0530

    Init repo: Sample starter files

---
+++ pre-requisites.md
@@ -0,0 +1,25 @@
+let, var, const
+REST operator and spread parameters
+
+class
+super
+jsx  (React.createElement)
+virtual dom
+reconciliation/diffing
+
+
+function Task () {
+
+    this.addTask = function () {
+
+    }
+    this.removeTask = function () {
+
+    }
+}
+
+Task.prototype.addTask = function () {
+    
+}
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.onTodoRemove = this.onTodoRemove.bind(this);
+    this.onTaskAdd = this.onTaskAdd.bind(this);
+    this.state = {
+      tasks: [
+        { id: 1, "title": "Task 1", done: false },
+        { id: 2, "title": "Task 2", done: true },
+        { id: 3, "title": "Task 3", done: false },
+      ]
+    }
+  }
+
+  onTaskAdd() {
+    let newTask = {
+      id: this.state.tasks.length + 1,
+      title: +new Date(),
+      done: false
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
+  onTodoRemove(id) {
+    let tasks = this.state.tasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    this.setState({
+      tasks: tasks
+    });
+  }
+
   render() {
+    let taskUI = this.state.tasks.map((task) => {
+      return (
+        <li key={task.id}>
+          <span>{task.title}
+            <button onClick={() => { this.onTodoRemove(task.id) }} >X</button></span>
+        </li>
+      );
+    });
+
+    console.log(taskUI);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Task App</h2>
+        <button onClick={this.onTaskAdd}>New Task</button>
+        <ul>
+          {taskUI}
+        </ul>
       </div>
     );
   }
